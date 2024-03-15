39a2a45cf4c28776e8fcfa1ef4849712e7c1ef7d
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-15 13:44:20 +1000

    LAB - Добавил переменные

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+	static void Main()
+	{
+		int a = 4;
+		float b = 3f;
+		decimal c = 3.09m;
+		string str = "String";
+		Console.WriteLine("Hello World");
+	}
+}
