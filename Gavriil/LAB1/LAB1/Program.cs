1966f5d007a3a13962d30d3b31590e7163db7c2a
Gavriilbolt <barogavrooon@gmail.com>
2024-03-16 11:39:50 +1000

    Выполненная лабораторная работа 1
    Выполненная лабораторная работа 1

---
+++ Gavriil/LAB1/LAB1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LR_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab # 1");
+            Console.WriteLine();
+            Console.WriteLine("by Gavriil 29.07.2002 town: Birobidzhan");
+            Console.WriteLine("physical culture is my favorite school subject ");
+            Console.WriteLine("I like coding, math and study chinesse language ");
+            Console.WriteLine("1201 grup");
+            Console.WriteLine("The structure of console app");
+            Console.WriteLine("");
+            
+            int a = 123;
+            int b = 7;
+            int w111 = 1000;
+            int t = 5;
+            int w = 38;
+            int y = 42;
+            int x = 5000;
+
+            string str = "hello, World";
+
+            Console.WriteLine("varible a is {0}", a);
+            Console.WriteLine("varible b is {0}", b);
+            Console.WriteLine("varible str is {0}", str);
+            Console.WriteLine("Variant two");
+
+            int Se = w111 + b * t - x + y * w;
+            Console.WriteLine(Se);
+
+
+
+
+            Console.ReadKey();
+            
+            
+            Console.WriteLine("For closing press any key");
+
+        }
+
+
+    }
+
+
+}
