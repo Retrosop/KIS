eb7f7f93d63fc9be602e3512a49a6ad17bf0ce68
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-29 14:19:40 +1000

    LAB - Добавление лаб. работы 2

---
+++ checherina.e/lab2/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+	public class Ball
+	{
+		private double r = 0;
+
+		public double R {  
+			get { return r; }
+			set {  
+				if (value > 0)
+					r = value;
+				else
+					Console.WriteLine("Радиус сферы должен быть положительным");
+				
+			} 
+		}
+        public Ball()
+        {
+        }
+		public Ball(double radius)
+		{
+			this.r = radius;
+
+		}
+		public void ballInfo()
+		{
+            Console.WriteLine($"Шар с радиусом {r}");
+        }
+		public double ballDiametr()
+		{
+			return r * 2;
+		}
+        public double ballSquare()
+		{
+			return r * r * 4 * double.Pi;
+		}
+
+		public double ballVolume() {
+			return r * r * r * 4/3 * double.Pi;
+		}
+    }
+}
---
+++ checherina.e/lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace lab2
+{
+	class MyProgram
+	{
+		static void Main()
+		{
+			Ball ball1 = new Ball();
+
+			Ball ball2 = new Ball(4);
+			ball2.ballInfo();
+			ball2.ballDiametr();
+			var diametr = ball2.ballDiametr();
+			var pl = ball2.ballSquare();
+			var ob = ball2.ballVolume();
+			Console.WriteLine($"Диаметр шара {diametr}");
+            Console.WriteLine($"Площадь шара {pl}");
+			Console.WriteLine($"Объём шара {ob}");
+        }
+	}
+}
