66b2b467ce9ea3523af5c36251e49ce0c33e34c7
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-29 12:32:12 +1000

    LAB - Добавлена лаб. работа №1

---
+++ checherina.e/lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LR_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1. Разработка консольного приложения");
+            Console.WriteLine("ФИО: Чечерина Екатерина Юрьевна");
+            Console.WriteLine("Группа: 1201. 44.03.05");
+            Console.WriteLine("Дата рождения: 29.07.2002");
+            Console.WriteLine("г. Биробиджан");
+            Console.WriteLine("Любимый предмет в школе: Математика");
+            Console.WriteLine("Хобби: танцы");
+
+            Console.ReadKey();
+        }
+    }
+}
