dff64230b22595f3ba43f5d2eac3307c84272a4c
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-04-26 13:03:39 +1000

    LAB - соединение с базой данной Access EntityFrameworkCore.Jet System.Data.OleDb

---
+++ glagolev.v/ConsoleAppAccess/DBModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using EntityFrameworkCore.Jet;
+
+namespace ConsoleAppAccess
+{
+	internal class DBModel
+	{
+		public class Client
+		{
+			[Key]
+			public int Id { get; set; }
+			public string? NameClient{ get; set; }
+			public string? Adress { get; set; }
+			public string? Telef { get; set; }
+			public string? Email { get; set; }
+		}
+		public class DataContext : DbContext
+		{
+			public DbSet<Client> Client { get; set; }
+
+			protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			{
+				optionsBuilder.UseJet(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\kis\sklad.mdb;");
+			}
+		}
+	}
+}
+
+
---
+++ glagolev.v/ConsoleAppAccess/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using static ConsoleAppAccess.DBModel;
+
+
+namespace ConsoleAppAccess
+{
+	//class Operate
+	//{
+	//	public void Add(DataContext db, string fio, DateTime dateroj, int age, bool pol)
+	//	{
+	//		Sotrudnik sotr = new Sotrudnik() { Fio = fio, Age = age, Dater = dateroj, Pol = pol };
+	//		db.Sotrudnik.Add(sotr);
+	//		db.SaveChanges();
+	//	}
+	//}
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			//	Добавление
+			using (DataContext db = new DataContext())
+			{
+				//add
+
+				//Client cl1 = new Client();
+				//cl1.NameClient = "Максимов Степан";
+				//cl1.Telef = "41112";
+				//cl1.Adress = "Широкая 32";
+				//cl1.Email = "re@mail.ru";
+
+
+				//db.Client.Add(cl1);
+				//db.SaveChanges();
+
+
+				//update
+				Client s2 = db.Client.SingleOrDefault(s => s.Id == 1);
+				if (s2 != null)
+				{
+					s2.Telef = "454545";
+					db.SaveChanges();
+				}
+
+				//delete
+
+				Client s1 = db.Client.SingleOrDefault(s => s.Id == 2);
+				if (s1 != null)
+				{
+					//удаляем объект
+					db.Client.Remove(s1);
+					db.SaveChanges();
+				}
+
+
+				//select
+				var sall = db.Client.Select(x => x.Telef);
+				foreach (var s in sall)
+				{
+					Console.WriteLine($"{s}");
+				}
+
+				var sall1 = db.Client;
+				foreach (var xs in sall1)
+				{
+					Console.WriteLine($"{xs.NameClient} - {xs.Telef}");
+				}
+				Console.WriteLine("HW");
+			}
+		}
+	}
+}
