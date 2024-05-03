bc5bd9b63ea5d14fbe4d91d7cf379b7bbba257c5
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-05-03 12:34:26 +1000

    LAB - Добавлена таблица tovar и prihod

--- glagolev.v/ConsoleAppAccess/DBModel.cs
+++ glagolev.v/ConsoleAppAccess/DBModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using EntityFrameworkCore.Jet;
+using static ConsoleAppAccess.DBModel;
 
 namespace ConsoleAppAccess
 {
@@ -9,12 +10,30 @@ namespace ConsoleAppAccess
 		public class Client
 		{
 			[Key]
-			public int Id { get; set; }
-			public string? NameClient{ get; set; }
+			public int IdClient { get; set; }
+			public string Name{ get; set; }
 			public string? Adress { get; set; }
 			public string? Telef { get; set; }
 			public string? Email { get; set; }
 		}
+		public class Tovar
+		{
+			[Key]
+			public int IdTovar { get; set; }
+			public string Name { get; set; }
+			public string Edizm { get; set; }
+			public decimal Zena { get; set; }
+		}
+		public class Prihod
+		{
+			[Key]
+			public int IdPrihod { get; set; }
+			public int IdTovar { get; set; }
+			public int IdPostav { get; set; }
+			public DateTime DatPrih { get; set; }
+			public int Kolvo { get; set; }
+		}
+
 		public class DataContext : DbContext
 		{
 			public DbSet<Client> Client { get; set; }
