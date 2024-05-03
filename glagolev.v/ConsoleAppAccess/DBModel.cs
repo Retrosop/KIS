ca10160f32add5b87cb0ccb74c62dc59aae885c3
Gavriilbolt <barogavrooon@gmail.com>
2024-05-03 12:34:28 +1000

    Таблица Postav - добавлена

--- glagolev.v/ConsoleAppAccess/DBModel.cs
+++ glagolev.v/ConsoleAppAccess/DBModel.cs
@@ -15,7 +15,28 @@ namespace ConsoleAppAccess
 			public string? Telef { get; set; }
 			public string? Email { get; set; }
 		}
-		public class DataContext : DbContext
+		/*
+        Название поля   Смысл					Тип			Длина
+			IdPostav    Номер поставщика		Int			11
+			Postav		Наименование поставщика VarChar		50
+			Gorod		Город поставщика		VarChar		20
+			Ulica		Улица поставщика		VarChar		20
+			Telef		Телефон поставщика		VarChar		15
+
+		*/
+
+		public class Postav
+		{
+			public int IdPostav { get; set; }
+			public string? NamePostav { get; set; }
+            public string? Gorod { get; set; }
+            public string? Ulica { get; set; }
+            public string? Telef { get; set; }
+
+
+        }
+
+        public class DataContext : DbContext
 		{
 			public DbSet<Client> Client { get; set; }
 
