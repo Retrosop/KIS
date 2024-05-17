2693c31add64100962bfcd812df7794474c92b79
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-05-17 12:11:47 +1000

    LAB - Добавление приложения

---
+++ checherina.e/AppSklad/ProductForm.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.ObjectModel;
+using System.Printing;
+using System.Windows;
+using static ConsoleAppAccess.DBModel;
+
+namespace AppSklad
+{
+	/// <summary>
+	/// Логика взаимодействия для ProductForm.xaml
+	/// </summary>
+	public partial class ProductForm : Window
+	{
+		int IdSelected = 0;
+		ObservableCollection<Product> products;
+		public ProductForm()
+		{
+			InitializeComponent();
+			products = new ObservableCollection<Product>();
+			productList.SelectedIndex = IdSelected;
+
+			using (DataContext db = new DataContext())
+			{
+				var glist = db.Product.ToList();
+				foreach (Product xproduct in glist)
+				{
+					products.Add(xproduct);
+				}
+
+				productList.ItemsSource = products;
+				if (IdSelected != 0)
+				{
+					var t = db.Product
+						.Where(c => c.Id == (productList.SelectedItems[0] as Product).Id)
+						.FirstOrDefault();
+					InputName.Text = t.Name;
+					InputMeasurement.Text = t.Measurement;
+					InputPrice.Text = t.Price.ToString().Replace(',','.');
+				}
+			}
+		}
+
+		private void addProduct_Click(object sender, RoutedEventArgs e)
+		{
+			using (DataContext db = new DataContext())
+			{
+				Product product = new Product();
+				product.Name = InputName.Text;
+				product.Measurement = InputMeasurement.Text;
+				product.Price = float.Parse(InputPrice.Text.Replace('.',','));
+
+				try
+				{
+					db.Product.Add(product);
+					db.SaveChanges();
+				}
+				catch (FormatException)
+				{
+					//В случае появление ошибок ввожда
+				}
+				//==================
+				products.Clear();
+				var productQuery = db.Product.ToList();
+				foreach (Product xproduct in productQuery)
+				{
+					products.Add(xproduct);
+				}
+				productList.ItemsSource = products;
+				//==================
+
+			}
+		}
+
+		private void productList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			IdSelected = productList.SelectedIndex;
+
+			using (var db = new DataContext())
+			{
+				if (productList.SelectedIndex == -1)
+				{
+					MessageBox.Show("Выделите запись");
+				}
+				else
+				{
+					Product t = db.Product
+									.Where(c => c.Id == (productList.SelectedItems[0] as Product).Id).FirstOrDefault();
+					InputName.Text = t.Name;
+					InputMeasurement.Text = t.Measurement;
+					InputPrice.Text = t.Price.ToString().Replace(',', '.'); ;
+				}
+			}
+		}
+
+		private void deleteProduct_Click(object sender, RoutedEventArgs e)
+		{
+			if (productList.SelectedIndex == -1)
+			{
+				MessageBox.Show("Выделите запись");
+			}
+			else
+			{
+				MessageBoxResult dialogResult = MessageBox.Show("Вы действительно хотите удалить запись"
+					, "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+				if (dialogResult == MessageBoxResult.Yes)
+				{
+					using (var db = new DataContext())
+					{
+						Product t = db.Product
+										.Where(c => c.Id == (productList.SelectedItems[0] as Product).Id).FirstOrDefault();
+						db.Product.Remove(t);
+						db.SaveChanges();
+
+						//==================
+						products.Clear();
+						var glist = db.Product.ToList();
+						foreach (Product xproduct in glist)
+						{
+							products.Add(xproduct);
+						}
+						productList.ItemsSource = products;
+						//==================
+					}
+				}
+			}
+		}
+
+		private void updateProduct_Click(object sender, RoutedEventArgs e)
+		{
+			if (productList.SelectedIndex == -1)
+			{
+				MessageBox.Show("Выделите запись");
+			}
+			else
+			{
+				using (var db = new DataContext())
+				{
+					Product t = db.Product
+									.Where(c => c.Id == (productList.SelectedItems[0] as Product).Id).FirstOrDefault();
+
+					t.Name = InputName.Text;
+					t.Measurement = InputMeasurement.Text;
+					t.Price = float.Parse(InputPrice.Text.Replace('.', ','));
+
+
+					db.SaveChanges();
+
+					//==================
+					products.Clear();
+					var glist = db.Product.ToList();
+					foreach (Product xproduct in glist)
+					{
+						products.Add(xproduct);
+					}
+					productList.ItemsSource = products;
+					//==================
+				}
+			}
+		}
+	}
+}
---
+++ checherina.e/ConsoleAppAccess/DBModel.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using EntityFrameworkCore.Jet;
+using static ConsoleAppAccess.DBModel;
+
+namespace ConsoleAppAccess
+{
+	public class DBModel
+	{
+		public class ComboboxItem
+		{
+			public string Text { get; set; }
+			public int Value { get; set; }
+
+			public override string ToString()
+			{
+				return Text;
+			}
+			public string ToKey()
+			{
+				return Value.ToString();
+			}
+		}
+		public class Client
+		{
+			[Key]
+			public int Id { get; set; }
+			public string? NameClient { get; set; }
+			public string? Adress { get; set; }
+			public string? Telef { get; set; }
+			public string? Email { get; set; }
+			public DateTime? DateBirthday { get; set; }
+			public bool Gender { get; set; }
+
+		}
+		//Поставщик продукции
+		public class Supplier
+		{
+			[Key]
+			public int Id { get; set; }
+			//Название поставщика
+			public string Name { get; set; }
+			//Город
+			public string? City { get; set; }
+			//Улица
+			public string? Street { get; set; }
+			//Телефон
+			public string? Phone { get; set; }
+
+		}
+		public class Product
+		{
+			[Key]
+			public int Id { get; set; }
+			//Название товара
+			public string Name { get; set; }
+			//Единица измерения товара
+			public string Measurement { get; set; }
+			//Цена товара
+			public float Price { get; set; }
+		}
+		public class Prihod
+		{
+			[Key]
+			public int Id { get; set; }
+			public int IdTovar { get; set; }
+			public int IdPostav { get; set; }
+			public DateTime DatPrih { get; set; }
+			public int Kolvo { get; set; }
+		}
+
+		public class DataContext : DbContext
+		{
+			public DbSet<Client> Client { get; set; }
+			public DbSet<Product> Product { get; set; }
+			public DbSet<Supplier> Supplier { get; set; }
+
+			protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			{
+				optionsBuilder.UseJet(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\kis\checherina.e\sklad.mdb;");
+			}
+		}
+	}
+}
+
+
