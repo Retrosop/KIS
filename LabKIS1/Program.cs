730c2ae3b6bca4114a1f9a901505c9ea3d2339e1
DaryaAristova <163491522+DaryaAristova@users.noreply.github.com>
2024-05-31 23:49:40 +1000

    Add files via upload

---
+++ LabKIS1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №1");
+            Console.WriteLine("Лабораторная работа №1: Разработка консольного приложения");
+            Console.WriteLine("Выполнила: Аристова Дарья Андреевна");
+            Console.WriteLine("Группа: 1201. Шифр специальности: 44.03.5 Педагогическое образование");
+            Console.WriteLine("Дата рождения: 23.05.2002");
+            Console.WriteLine("Населённый пункт: г. Биробиджан");
+            Console.WriteLine("Любимый предмет в школе: Математика");
+            Console.WriteLine("Хобби и интересы: Чтение книг\n");
+
+            Console.WriteLine("Задание №2. Вариант 1");
+            //F = a_1 + b - a * (x + y5)
+            double F = 0;
+            int a_1 = 4;
+            double b = 1.6f, a = 67438.3f;
+            long x = 199, y5 = 512;
+            F = a_1 + b - a * (x + y5);
+            Console.WriteLine("Результат вычисления выражения F = " + F);
+        }
+    }
+}
---
+++ LabKIS2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+namespace Lab2
+{
+    class Par
+    {
+        private double a, b, c;
+        public double GetV()
+        {
+            return a * b * c;
+        }
+
+        public double GetS()
+        {
+            return 2 * (a * b + b * c + a * c);
+        }
+
+        public void Info()
+        {
+            String str = "Информация о параллелепипеде";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format(
+                "A = {0:0.000}, B = {1:0.000}, C = {2:0.000}", a, b, c));
+            Console.WriteLine(string.Format("V = {0:0.000}", GetV()));
+            Console.WriteLine(string.Format("S = {0:0.000}", GetS()));
+        }
+
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+    class Sphere
+    {
+        //V = 4/3 * Pi * R^3
+        //S = 4 * Pi * R^2
+        private double R;
+
+        public double Volume()
+        {
+            double V = 4 / 3 * Math.PI * Math.Pow(R, 3);
+            return V;
+        }
+
+        public double Square()
+        {
+            double S = 4 * Math.PI * Math.Pow(R,2);
+            return S;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Информация о шаре");
+            Console.WriteLine(string.Format("Радиус шара R = {0:0.00}, диаметр шара D = {1:0.00}", R, R*2));
+            Console.WriteLine(string.Format("Объём шара: {0:0.00}", Volume()));
+            Console.WriteLine(string.Format("Площадь шара: {0:0.00}", Square()));
+        }
+
+        public void Load()
+        {
+            R = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetIn(new_in);
+            Console.SetOut(new_out);
+#endif
+            Par p = new Par();
+            p.Load();
+            p.Info();
+            Console.WriteLine("\n");
+            Sphere s = new Sphere();
+            s.Load();
+            s.Info();
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
