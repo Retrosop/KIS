fea9e3511cf295e43ed235690899ca7febddd924
ArtLut <166784065+ArtLut@users.noreply.github.com>
2024-06-03 11:52:50 +1000

    - new com

--- lutaenko.a/lab1/Program.cs
+++ lutaenko.a/lab1/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-class MyProgram
+namespace LR_1
 {
-	static void Main()
-	{
-		int a = 5;
-		Console.WriteLine("Hello world");
-		Console.WriteLine("Hello world");
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("");
+            Console.WriteLine("Выполнили: Артём Лутаенко и Александра Заяц");
+            Console.WriteLine("Группа: 1201");
+            Console.WriteLine("Наименование ЛР: Структура консольного прмиложения");
+            Console.WriteLine("");
+            Console.WriteLine("Для завершения работы программы нажмите любую клавишу...");
+
+            Console.ReadKey();
+        }
+    }
 }
